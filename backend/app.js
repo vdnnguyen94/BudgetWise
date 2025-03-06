4d1d5910e69ba00fae5e349d512a87d18e567bc7
Van Nguyen <vdnnguyen94@gmail.com>
2025-03-06 11:37:16 -0500

    ReadMe Instruction

--- backend/app.js
+++ backend/app.js
@@ -34,4 +34,7 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Local: http://localhost:${PORT}`);
+});
