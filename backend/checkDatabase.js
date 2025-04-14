fa0dc79d19f67e7b24876db4647b1a3d5f95ff9b
rbatool4 <syedarimmsha21@gmail.com>
2025-04-13 23:41:31 -0400

    Removed all commented-out code

--- backend/checkDatabase.js
+++ backend/checkDatabase.js
@@ -3,37 +3,14 @@ import mongoose from 'mongoose';
 const mongoURI = "mongodb+srv://syedarimmsha21:FF9SmmtFNFguLNQA@income.xqdk.mongodb.net/BudgetWiseIncomeDB?retryWrites=true&w=majority"
 
 
-//const mongoURI = "mongodb+srv://ypate116:6duz2JghaKygxJMc@cluster0.k2rtz.mongodb.net/budget?retryWrites=true&w=majority"; // Replace with your connection string
+// OR use this const mongoURI = "mongodb+srv://ypate116:6duz2JghaKygxJMc@cluster0.k2rtz.mongodb.net/budget?retryWrites=true&w=majority"; // Replace with your connection string
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(async () => {
     console.log("Connected to MongoDB");
 
-    // // Define the Budget & Expense schemas
-    // const Budget = mongoose.model("Budget", new mongoose.Schema({ userId: String, totalBudget: Number }));
-    // const Expense = mongoose.model("Expense", new mongoose.Schema({ userId: String, amount: Number }));
-
-    // // Query for any existing Budget and Expenses
-    // const budgetExists = await Budget.exists({});
-    // const expenseExists = await Expense.exists({});
-
-    // console.log("📌 Budget Exists:", budgetExists ? "Yes" : "No");
-    // console.log("📌 Expense Exists:", expenseExists ? "Yes" : "No");
-
-
-    // mongoose.connection.close();
-
-    // const Budget = mongoose.model("Budget", new mongoose.Schema({ userId: String, totalBudget: Number, categories: Array }));
-    // const Expense = mongoose.model("Expense", new mongoose.Schema({ userId: String, amount: Number, category: String, date: Date }));
-
-    // // Fetch first 5 budgets and expenses
-    // const budgets = await Budget.find().limit(5);
-    // const expenses = await Expense.find().limit(5);
-
-    // console.log("📊 Budgets:", budgets);
-    // console.log("📊 Expenses:", expenses);
-
+ 
     // Define models
     const Budget = mongoose.model("Budget", new mongoose.Schema({}));
     const Expense = mongoose.model("Expense", new mongoose.Schema({}));
