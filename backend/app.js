8f6cb92414abe789cbf823dedee0cc9ce2c1fc0d
Van Nguyen <vdnnguyen94@gmail.com>
2025-09-11 17:09:32 -0400

    COR OPRTIONS

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options('*', cors());
 app.use(cookieParser());
 
 // --- Root route for testing ---
