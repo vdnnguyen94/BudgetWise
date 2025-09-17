f1d29450f7a0498879f241ea0b003e3f952d5bc6
Van Nguyen <vdnnguyen94@gmail.com>
2025-09-17 15:07:15 -0400

    fix Lambda timeout

--- backend/handler.js
+++ backend/handler.js
@@ -6,7 +6,9 @@ import connectToDatabase from './db.js'; // <-- Import the helper
 const serverlessApp = serverless(appJs);
 
 export const app = async (event, context) => {
-  // Wait for the DB connection before running the app
-  await connectToDatabase();
+  // Only connect when not running offline
+  if (!process.env.IS_OFFLINE) {
+    await connectToDatabase();
+  }
   return await serverlessApp(event, context);
 };
